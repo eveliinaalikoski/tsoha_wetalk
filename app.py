1569fc6ce36d1bb9d07c088c1e75a085f637e9d1
alev <alev@ad.helsinki.fi>
2023-11-15 18:44:06 +0200

    muokkausta app.py

--- app.py
+++ app.py
@@ -1,7 +1,33 @@
 from os import getenv
+from app import app
 from flask import Flask
+from flask import render_template, request, redirect
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.sql import text
+from db import db
 
 app = Flask(__name__)
 app.secret_key=getenv("SECRET_KEY")
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql:///alev"
+db=SQLAlchemy(app)
 
-import routes
+@app.route("/")
+def index():
+	return render_template("index.html", message="Welcome!")
+
+@app.route("/login", methods=["POST"])
+def login():
+	
+	return render_template("login.html")
+
+@app.route("/register", methods=["POST"])
+def register():
+	return render_template("register.html")
+	
+@app.route("/front_page", methods=["POST", "GET"])
+def front_page():
+	username=request.form["username"]
+	groups=db.session.execute(text("SELECT group_name FROM groups"))
+	return render_template("front_page.html",
+						username=username,
+						groups=groups)
